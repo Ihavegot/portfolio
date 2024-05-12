7877ea2fd087d81c16373606ada825d9c4ae6bd1
Ihavegot <lukasz.bocianski1112@gmail.com>
2024-05-12 20:32:11 +0200

    add: aboutme component base

--- src/app/about-me/about-me.component.ts
+++ src/app/about-me/about-me.component.ts
@@ -1,10 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-about-me',
   templateUrl: './about-me.component.html',
   styleUrl: './about-me.component.css'
 })
-export class AboutMeComponent {
-
+export class AboutMeComponent implements OnInit{
+  constructor(){}
+  aboutMeData: string[] = [
+    "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+    "Libero nunc consequat interdum varius sit amet mattis. Eget gravida cum sociis natoque penatibus.",
+    "Vulputate dignissim suspendisse in est ante in. Non arcu risus quis varius quam. Ultrices vitae auctor eu augue ut. Risus commodo viverra maecenas accumsan lacus vel facilisis volutpat est."
+  ];
+  interests: string[] = [
+    "Football",
+    "Games",
+    "Movies",
+    "RPG",
+    "Fantasy",
+    "Gatcha"
+  ];
+  ngOnInit(): void {
+    
+  }
 }
